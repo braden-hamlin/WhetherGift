baf58a09756c453a358723cf3e3f566220a5bdc9
Braden Hamlin <braden@placefull.com>
2019-11-17 14:52:54 -0800

    7.6 complete

--- WhetherGift/PageVC.swift
+++ WhetherGift/PageVC.swift
@@ -53,12 +53,26 @@ class PageVC: UIPageViewController {
         
         listButton.setImage(UIImage(named: "listbutton"), for: .normal)
         listButton.setImage(UIImage(named: "listbutton-highlighted"), for: .highlighted)
-        listButton.addTarget(self, action: #selector(segueToLocationVC), for: .touchUpInside)
+        listButton.addTarget(self, action: #selector(segueToListVC), for: .touchUpInside)
         view.addSubview(listButton)
     }
+    //MARK:- Segues
+    @objc func segueToListVC() {
+        performSegue(withIdentifier: "ToListVC", sender: nil)
+    }
     
-    @objc func segueToLocationVC() {
-        print("Hey! You Clicked Me")
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToListVC" {
+            let destination = segue.destination as! ListVC
+            destination.locationsArray = locationsArray
+            destination.currentPage = currentPage
+        }
+    }
+    
+    @IBAction func unwindFromListVC(sender: UIStoryboardSegue) {
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        setViewControllers([createDetailVC(forPage: currentPage)], direction: .forward, animated: false, completion: nil)
     }
     
     //MARK:- Create View Controller for UIPageViewContoller
