22d57034298569df3a72ba3a76ae746886266c48
Braden Hamlin <braden@placefull.com>
2019-11-20 12:43:17 -0800

    7.19 complete

--- WhetherGift/DetailVC.swift
+++ WhetherGift/DetailVC.swift
@@ -38,13 +38,23 @@ class DetailVC: UIViewController {
     }
     
     func updateUserInterface() {
-        locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
-        tempertureLabel.text = locationsArray[currentPage].currentTemp
-        summaryLabel.text = locationsArray[currentPage].currentSummary
-        currentImage.image = UIImage(named: locationsArray[currentPage].currentIcon)
+        let location = locationsArray[currentPage]
+        locationLabel.text = location.name
+        let dateString = formalTimeForTimeZone(unixDate: location.currentTime, timeZone: location.timeZone)
+        dateLabel.text = dateString
+        tempertureLabel.text = location.currentTemp
+        summaryLabel.text = location.currentSummary
+        currentImage.image = UIImage(named: location.currentIcon)
+        
     }
-    
+    func formalTimeForTimeZone(unixDate: TimeInterval, timeZone: String) -> String {
+        let usableDate = Date(timeIntervalSince1970: unixDate)
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM dd, y"
+        dateFormatter.timeZone = TimeZone(identifier: timeZone)
+        let dateString = dateFormatter.string(from: usableDate)
+        return dateString
+    }
 }
 
 extension DetailVC: CLLocationManagerDelegate {
@@ -78,7 +88,6 @@ extension DetailVC: CLLocationManagerDelegate {
         let currentLatitude = currentLocation.coordinate.latitude
         let currentLongitude = currentLocation.coordinate.longitude
         let currentCoordinates = "\(currentLatitude),\(currentLongitude)"
-        dateLabel.text = currentCoordinates
         geoCoder.reverseGeocodeLocation(currentLocation, completionHandler:
             {placemarks, error in
                 if placemarks != nil {
